968d0da7ab48b08c1a23e9173378982e8da70199
Le Duc Anh <alexdal1992@gmail.com>
2020-03-24 00:14:32 +0200

    the second commit

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -48,9 +48,9 @@ exports.signup = (req, res) => {
         .catch(err => {
             console.error(err);
             if(err.code === 'auth/email-already-in-use'){
-                return res.status(400).json({email: 'Email is already in use.'});
+                return res.status(400).json({ email: 'Email is already in use.' });
             }else{
-                return res.status(500).json({error: err.code});
+                return res.status(500).json({ general: 'Something went wrong. Please try again.' });
             }
         });
 };
@@ -96,6 +96,39 @@ exports.addUserDetails = (req, res) => {
         });
 };
 
+exports.getUserDetails = (req, res) => {
+    let userData = {};
+    db.doc(`/users/${req.params.handle}`).get()
+        .then(doc => {
+            if(doc.exists){
+                userData.user = doc.data();
+                return db.collection('screams').where('userHandle', '==', req.params.handle)
+                    .orderBy('createdAt', 'desc').get();
+            }else{
+                return res.status(404).json({ error: 'User not found' });
+            }
+        })
+        .then(data => {
+            userData.screams = [];
+            data.forEach(doc => {
+                userData.screams.push({
+                    body: doc.data().body,
+                    createdAt: doc.data().createdAt,
+                    userID: doc.data().userID,
+                    imageUrl: doc.data().imageUrl,
+                    likeCount: doc.data().likeCount,
+                    commentCount: doc.data().commentCount,
+                    screamID: doc.id
+                })
+            });
+            return res.json(userData);
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({ error: err.code });
+        })
+};
+
 exports.getAuthenticatedUser = (req, res) => {
     let userData = {};
     db.doc(`/users/${req.user.handle}`).get()
@@ -112,6 +145,21 @@ exports.getAuthenticatedUser = (req, res) => {
             });
             return db.collection('notifications').where('recipient', '==', req.user.handle)
                 .orderBy('createdAt', 'desc').limit(10).get();
+        })
+        .then(data => {
+            userNotifications = [];
+            data.forEach(doc => {
+                userData.notifications.push({
+                    recipient: doc.data().recipient,
+                    sender: doc.data().sender,
+                    createdAt: doc.data().createdAt,
+                    screamID: doc.data().screamID,
+                    type: doc.data().type,
+                    read: doc.data().read,
+                    notificationID: doc.id
+                })
+            });
+            return res.json(userData);
         })
         .catch(err => {
             console.error(err);
@@ -163,3 +211,19 @@ exports.uploadImage = (req, res) => {
     });
     busboy.end(req.rawBody);
 };
+
+exports.markNotificationsRead = (req, res) => {
+    let batch = db.batch();
+    req.body.forEach(notificationID => {
+        const notification = db.doc(`/notifications/${notificationID}`);
+        batch.update(notification, { read: true });
+    });
+    batch.commit()
+        .then(() => {
+            return res.json({ message: 'Notification marked read.' });
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({ error: err.code });
+        })
+}
